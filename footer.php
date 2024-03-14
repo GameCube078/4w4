7b682b865daeaa9f56392a37517d3458a5bf262e
GameCube078 <olivier.dorion@icloud.com>
2024-03-14 11:48:23 -0400

    ajout de footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer" class="global">
+            <footer>
+                <h2>Footer</h2>
+                <p>Texte de footer fort intéresant</p>
+                <p>poisson rouge de couleur bleu</p>
+            </footer>
+        </div>
+    </body>
+</html>
