893b2e9b6a9c8ae840cc6906a893c436690d57d6
GameCube078 <olivier.dorion@icloud.com>
2024-03-14 11:34:38 -0400

    s6c2 ajout de single.php et custom logo

--- single.php
+++ single.php
@@ -2,8 +2,8 @@
 
         <div id="entete" class="global">
             <header class="hero">
-                <h1><?php echo get_bloginfo("name") ?></h1>
-                <h2><?php echo get_bloginfo("description") ?></h2>
+                <h1>Thème WP</h1>
+                <h2>4W4 - Conception d'interface et développement Web</h2>
                 <h3>TIM-Collège Maisonneuve</h3>
                 <a href="https://en.wikipedia.org/wiki/Watermelon">
                     <button> Ceci est un lien </button>
@@ -30,40 +30,13 @@
                             <h3><?php substr(get_the_title(),0,7) ?></h3>
                             <h3><?php the_title();?></h3>
                             <p><?php echo wp_trim_words(get_the_content(),20);?></p>
-                            <a href="<?php  the_permalink()?>"> Article -></a>
                             </div>
                         <?php endwhile; ?>
                         <?php endif; ?>
                         </div>
-                        
-                        
-                
-            </section>
-        </div>
-        <div id="evenement" class="global diagonal">
-            <section>
-                <h2>Évènement</h2>
-                <p class="block-code">
-                    Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant Code intéresant
-                </p>
-            </section>
-            
-            </div>
-        </div>
         <div id="galerie" class="global">
             <section>
                 <h2>Galerie</h2>
-                <div class="divButton">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/tophat.png" alt="hat">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/bowlerhat.png" alt="hat">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/cowboyhat.png" alt="hat">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/sunhat.png" alt="hat">
-                </div>
-                <div class="divButton">
-                    <button> bouton 1 </button>
-                    <button> bouton 2 </button>
-                    <button> bouton 3 </button>
-                </div>
             </section>
             <?php get_template_part("gabarit/vague") ?>
         </div>
